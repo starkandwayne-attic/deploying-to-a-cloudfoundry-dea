c07e86d51031c4f452b2f0cf2506b49d91acecfa
Dr Nic Williams <drnicwilliams@gmail.com>
2012-11-11 21:43:14 -0800

    bin/start_app launches a sinatra app in the DEA

---
+++ bin/dea_discover.js
@@ -0,0 +1,21 @@
+var nats = require('nats').connect();
+
+// Simple Subscriber
+nats.subscribe('>', function(msg, reply, subject) {
+  console.log('Msg received on [' + subject + '] : ' + msg);
+});
+
+var message = {
+  'runtime_info': {
+    'name': 'ruby19',
+    'executable': 'ruby',
+    'version_output': 'ruby 1.9.3p286'
+  },
+  'limits': {
+    'mem': 256
+  },
+  'droplet': 'DROPLET_ID_1234'
+};
+nats.publish('dea.discover', JSON.stringify(message), function(response) {
+  console.log("Got dea.discover response: " + response);
+});
